80a1dbf372a4ac4b378bc2cdc29a276f2d4e6f6a
Inta <38420337+MyInta@users.noreply.github.com>
2020-01-12 23:24:36 +0800

    springcloud简单demo

---
+++ spring-cloud-01-eureka-customer-producer/spring-cloud-eureka-custmoer/src/main/java/cn/ssijri/springcloud/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package cn.ssijri.springcloud.controller;
+
+import cn.ssijri.springcloud.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author inta
+ * @date 2020/1/12
+ * @describe customer是顾客的意思，一半网上用consumer消费者表示
+ */
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private HelloRemote helloRemote;
+
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable(value = "name") String name) {
+        return helloRemote.index(name);
+    }
+
+}
